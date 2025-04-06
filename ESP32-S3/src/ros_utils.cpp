f05327983fee9d49e9f123abfc4af5ad1ef5ae10
PigLardLord <piglardlord@elkeproject.com>
2025-04-06 19:45:29 +0200

    Add retry mechanism to connect with agent

--- ESP32-S3/src/ros_utils.cpp
+++ ESP32-S3/src/ros_utils.cpp
@@ -1,5 +1,6 @@
 #include "ros_utils.h"
 #include <Arduino.h>
+#include <rmw_microros/ping.h>
 
 void error_loop() {
   while (true) {
@@ -7,6 +8,24 @@ void error_loop() {
     delay(500);
   }
 }
+
+void wait_for_agent()
+{
+  const int max_attempts = 120;
+  int attempt = 0;
+
+  while (rmw_uros_ping_agent(1000, 5) != RCL_RET_OK) {
+    Serial.println("Waiting for micro-ROS Agent...");
+    delay(1000);
+
+    attempt++;
+    if (attempt >= max_attempts) {
+      Serial.println("Agent not found, restarting ESP32...");
+      ESP.restart();  // Optional: restart if Agent is never found
+    }
+  }
+}
+
 
 void init_ros_base(
   rcl_allocator_t* allocator,
@@ -16,6 +35,10 @@ void init_ros_base(
   const char* node_name,
   size_t executor_handles
 ) {
+  // Wait for micro-ROS Agent
+  wait_for_agent();
+
+  // Initialize ROS base
   *allocator = rcl_get_default_allocator();
   RCCHECK(rclc_support_init(support, 0, NULL, allocator));
   RCCHECK(rclc_node_init_default(node, node_name, "", support));
