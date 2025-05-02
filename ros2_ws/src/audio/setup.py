dbc5b952152be99f10bce3792a2110d7dcb699d6
PigLardLord <piglardlord@elkeproject.com>
2025-05-02 13:31:49 +0200

    Move porcupine wake-up file on resources sub-folder

--- ros2_ws/src/audio/setup.py
+++ ros2_ws/src/audio/setup.py
@@ -1,19 +1,22 @@
+from setuptools import setup
 from glob import glob
-from setuptools import find_packages, setup
+import os
 
 package_name = 'audio'
 
 setup(
     name=package_name,
     version='0.0.0',
-    packages=['audio'],
+    packages=[package_name],
     package_dir={'': 'src'},
     data_files=[
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
-        ('share/' + package_name + '/resources', glob('resources/*.ppn')),
+        (os.path.join('share', package_name, 'resources'),
+            glob(os.path.join('src', package_name, 'resources', '*.ppn')))
     ],
+    include_package_data=True,
     include_package_data=True,
     install_requires=['setuptools'],
     zip_safe=True,
@@ -21,7 +24,6 @@ setup(
     maintainer_email='piglardlord@elkeproject.com',
     description='Voice interface for Elke robot',
     license='Creative Commons Attribution-NonCommercial-ShareAlike 4.0',
-    tests_require=['pytest'],
     entry_points={
         'console_scripts': [
             'wake_listener = audio.elke_listener:main',
