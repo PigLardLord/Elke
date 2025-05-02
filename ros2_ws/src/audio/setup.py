481611497d6c53e1bb51726b10b90fb0045299a4
PigLardLord <piglardlord@elkeproject.com>
2025-05-02 19:47:57 +0200

    Import all packages excluding test

--- ros2_ws/src/audio/setup.py
+++ ros2_ws/src/audio/setup.py
@@ -7,8 +7,7 @@ package_name = 'audio'
 setup(
     name=package_name,
     version='0.0.0',
-    packages=find_packages(where='src'),
-    package_dir={'': 'src'},
+    packages=find_packages(exclude=['test']),
     data_files=[
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
