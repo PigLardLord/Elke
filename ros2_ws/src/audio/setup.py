e836b3a4fe259ea2076eb77ec8065ff28149d153
PigLardLord <piglardlord@elkeproject.com>
2025-05-05 18:18:17 +0200

    Make it simple.

--- ros2_ws/src/audio/setup.py
+++ ros2_ws/src/audio/setup.py
@@ -7,18 +7,12 @@ package_name = 'audio'
 setup(
     name=package_name,
     version='0.0.0',
-    packages=find_packages(where='src', exclude=['test']),
-    package_dir={'': 'src'},
+    packages=[package_name],
     data_files=[
-        ('share/ament_index/resource_index/packages',
-            [f'resource/{package_name}']),
-        (f'share/{package_name}', ['package.xml']),
-        (
-            f'share/{package_name}/resources',
-            glob(f'src/{package_name}/{package_name}/resources/*.ppn')
-        ),
+        ('share/ament_index/resource_index/packages',['resource/{package_name}']),
+        ('share/{package_name}', ['package.xml']),
+        ('share/{package_name}/resources',glob('resources/*.ppn')),
     ],
-    include_package_data=True,
     install_requires=['setuptools'],
     zip_safe=True,
     maintainer='master',
