d8bbfdbef811f7d7712d3c93e996f5de4a215b1d
PigLardLord <piglardlord@elkeproject.com>
2025-05-05 18:25:32 +0200

    Change the way strings are concatenated.

--- ros2_ws/src/audio/setup.py
+++ ros2_ws/src/audio/setup.py
@@ -7,11 +7,11 @@ package_name = 'audio'
 setup(
     name=package_name,
     version='0.0.0',
-    packages=[package_name],
+    packages=find_packages(exclude=['test']),
     data_files=[
-        ('share/ament_index/resource_index/packages',['resource/{package_name}']),
-        ('share/{package_name}', ['package.xml']),
-        ('share/{package_name}/resources',glob('resources/*.ppn')),
+        ('share/ament_index/resource_index/packages', ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+        ('share/' + package_name + '/resources', glob('resources/*.ppn')),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
