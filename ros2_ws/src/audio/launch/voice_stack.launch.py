e1b04991ef72a4142691d0e59a42851353d2dea1
PigLardLord <piglardlord@elkeproject.com>
2025-05-06 13:13:02 +0200

    Implement launcher

---
+++ ros2_ws/src/audio/launch/voice_stack.launch.py
@@ -0,0 +1,24 @@
+from launch import LaunchDescription
+from launch_ros.actions import Node
+
+def generate_launch_description():
+    return LaunchDescription([
+        Node(
+            package='audio',
+            executable='wake_listener_node',
+            name='wake_word_node',
+            output='screen'
+        ),
+        Node(
+            package='audio',
+            executable='voice_session_manager',
+            name='voice_session_manager',
+            output='screen'
+        ),
+        Node(
+            package='audio',
+            executable='stt_whisper_node',
+            name='stt_whisper',
+            output='screen'
+        )
+    ])
